6ad4a09546a7e0f123b7133c397b03df311dc603
Jarek Jarcec Cecho <jarcec@apache.org>
2017-12-21 16:05:01 -0800

    Initial version of the topology

---
+++ README.rst
@@ -0,0 +1,59 @@
+==================================================
+Confluent Schema Registry topology for clusterdock
+==================================================
+
+This repository houses the **Confluent Schema Registry** topology for `clusterdock`_.
+
+.. _clusterdock: https://github.com/clusterdock/clusterdock
+
+Usage
+=====
+
+Assuming you've already installed **clusterdock** (if not, go `read the docs`_),
+you use this topology by cloning it to a local folder and then running commands
+with the ``clusterdock`` script:
+
+.. _read the docs: http://clusterdock.readthedocs.io/en/latest/
+
+.. code-block:: console
+
+    $ git clone https://github.com/clusterdock/topology_confluent_schema_registry
+    $ clusterdock start topology_confluent_schema_registry --confluent-version 4.0.0 --confluent-short-version 4.0
+
+To see full usage instructions for the ``start`` action, use ``-h``/``--help``:                                                 
+
+.. code-block:: console
+
+    $ clusterdock start topology_apache_kafka -h
+    usage: clusterdock start [--always-pull] [--namespace ns] [--network nw]
+                             [-o sys] [-r url] [-h] [--confluent-version ver]
+                             [--nodes node [node ...]]
+                             topology
+
+    Start a Confluent Schema Registry cluster
+
+    positional arguments:
+      topology              A clusterdock topology directory
+
+    optional arguments:
+      --always-pull         Pull latest images, even if they're available locally
+                            (default: False)
+      --namespace ns        Namespace to use when looking for images (default:
+                            None)
+      --network nw          Docker network to use (default: cluster)
+      -o sys, --operating-system sys
+                            Operating system to use for cluster nodes (default:
+                            None)
+      -r url, --registry url
+                            Docker Registry from which to pull images (default:
+                            docker.io)
+      -h, --help            show this help message and exit
+
+    Confluent Schema Registry arguments:
+      --confluent-version ver
+                            Confluent version (default: 4.0.0)
+
+    Node groups:
+      --nodes node [node ...]
+                            Nodes of the nodes group (default: ['registry-1'])
+
---
+++ start.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import tempfile
+import yaml
+from socket import getfqdn, socket
+
+from clusterdock.models import Cluster, Node
+from clusterdock.utils import wait_for_condition
+
+DEFAULT_NAMESPACE = 'clusterdock'
+REST_PORT = 8081
+
+logger = logging.getLogger('clusterdock.{}'.format(__name__))
+
+def success(time):
+    logger.info('Conditions satisfied after %s seconds.', time)
+
+
+def failure(timeout):
+    raise TimeoutError('Timed out after {} seconds waiting.'.format(timeout))
+
+
+# Validate that Zookeeper is up and running by connecting using shell
+def validate_zookeeper(node, quiet):
+    return node.execute('/confluent/bin/zookeeper-shell localhost:2181 ls /', quiet=quiet).exit_code == 0
+
+
+# Validate that Kafka is up by checking that all brokers are registered in zookeeper
+def validate_kafka(node, broker_count, quiet):
+    command = node.execute('/confluent/bin/zookeeper-shell localhost:2181 <<< "ls /brokers/ids" | tail -n 1', quiet=quiet)
+
+    if command.exit_code != 0:
+        return False
+
+    nodes = command.output
+    if not nodes.startswith('['):
+        return False
+
+    return len(json.loads(nodes)) == broker_count
+
+
+def main(args):
+    quiet = not args.verbose
+
+    # Image name
+    image = '{}/{}/topology_confluent_schema_registry:schema_registry-{}'.format(args.registry,
+                                                                                 args.namespace or DEFAULT_NAMESPACE,
+                                                                                 args.confluent_version)
+
+    # Nodes in the Kafka cluster
+    nodes = [Node(hostname=hostname,
+                  group='brokers',
+                  ports=[{REST_PORT : REST_PORT}],
+                  image=image)
+             for hostname in args.nodes]
+
+    cluster = Cluster(*nodes)
+    cluster.start(args.network, pull_images=args.always_pull)
+
+    # Create distributed zookeeper configuration
+    zookeeper_config = ('tickTime=2000\n'
+                        'dataDir=/zookeeper\n'
+                        'clientPort=2181\n'
+                        'initLimit=5\n'
+                        'syncLimit=2\n')
+    for idx, node in enumerate(cluster):
+        zookeeper_config += 'server.{}={}:2888:3888\n'.format(idx, node.hostname)
+
+    # Start all zookeepers
+    for idx, node in enumerate(cluster):
+        logger.info('Starting Zookeeper on node {}'.format(node.hostname))
+        node.execute('mkdir -p /zookeeper')
+        node.put_file('/zookeeper/myid', str(idx))
+        node.put_file('/zookeeper.properties', zookeeper_config)
+        node.execute('/start_zookeeper &', detach=True)
+
+    # Validate that Zookeepr is alive from each node
+    for node in cluster:
+        logger.info('Validating Zookeeper on node %s', node.hostname)
+        wait_for_condition(condition=validate_zookeeper,
+                           condition_args=[node, quiet],
+                           time_between_checks=3,
+                           timeout=60,
+                           success=success,
+                           failure=failure)
+
+    # Start all brokers
+    for idx, node in enumerate(cluster):
+        logger.info('Starting Kafka on node {}'.format(node.hostname))
+
+        kafka_config = node.get_file('/confluent/etc/kafka/server.properties')
+        kafka_config = kafka_config.replace('broker.id=0', 'broker.id={}'.format(idx))
+        node.put_file('/kafka.properties', kafka_config)
+
+        node.execute('/start_kafka &', detach=True)
+
+    # Verify that all Kafka brokers up
+    logger.info('Waiting on all brokers to register in zookeeper')
+    wait_for_condition(condition=validate_kafka,
+                       condition_args=[nodes[0], len(nodes), quiet],
+                       time_between_checks=3,
+                       timeout=60,
+                       success=success,
+                       failure=failure)
+
+    # Start schema registry on all nodes
+    for idx, node in enumerate(cluster):
+        logger.info('Starting Schema Registry on node {}'.format(node.hostname))
+        node.execute('/start_schema_registry &', detach=True)
